f41cfe68345d42be69f12c395a1b0f9e5eb377ad
Elena Garcia <elena.garcia@wizeline.com>
2022-12-21 16:44:51 +0100

    Add route to get internal port mapping of Product Service

--- ProductService/src/main/kotlin/com/wizeprojects/ellen/productservice/query/rest/ProductsQueryController.kt
+++ ProductService/src/main/kotlin/com/wizeprojects/ellen/productservice/query/rest/ProductsQueryController.kt
@@ -3,19 +3,24 @@ package com.wizeprojects.ellen.productservice.query.rest
 import com.wizeprojects.ellen.productservice.query.FindProductsQuery
 import org.axonframework.messaging.responsetypes.ResponseTypes
 import org.axonframework.queryhandling.QueryGateway
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
-@RequestMapping("/products")
-class ProductsQueryController(private val queryGateway: QueryGateway) {
 
+class ProductsQueryController(private val queryGateway: QueryGateway, private val env: Environment) {
+    @RequestMapping("/products")
     @GetMapping
     fun getProducts(): List<ProductRestModel> {
         return queryGateway.query(FindProductsQuery(),
             ResponseTypes.multipleInstancesOf(ProductRestModel::class.java)
         ).join()
+    }
+    @GetMapping
+    @RequestMapping("/port")
+    fun getPort(): String {
+        return "ON PORT ${env.getProperty("local.server.port")}"
     }
 }
