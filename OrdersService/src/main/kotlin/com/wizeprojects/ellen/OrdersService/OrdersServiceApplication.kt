0e9344ede607c18d9c4168495f42b64632961bb2
Elena Garcia <elena.garcia@wizeline.com>
2022-12-23 13:34:59 +0100

    Add new springBoot Kotlin service Orders with Axon, JPA & other dependencies

---
+++ OrdersService/src/main/kotlin/com/wizeprojects/ellen/OrdersService/OrdersServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.wizeprojects.ellen.OrdersService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrdersServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrdersServiceApplication>(*args)
+}
