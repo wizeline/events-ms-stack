be297029a151291a4b1e81b65a6ea07cd51f2bb9
Elena Garcia <elena.garcia@wizeline.com>
2022-12-28 10:07:25 +0100

    Add Saga handler

--- OrderService/src/main/kotlin/com/wizeprojects/ellen/orderservice/saga/OrderSaga.kt
+++ OrderService/src/main/kotlin/com/wizeprojects/ellen/orderservice/saga/OrderSaga.kt
@@ -1,6 +1,9 @@
 package com.wizeprojects.ellen.orderservice.saga
 
+import com.wizeprojects.ellen.orderservice.core.events.OrderCreatedEvent
 import org.axonframework.commandhandling.gateway.CommandGateway
+import org.axonframework.modelling.saga.SagaEventHandler
+import org.axonframework.modelling.saga.StartSaga
 import org.axonframework.spring.stereotype.Saga
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -11,4 +14,10 @@ class OrderSaga {
     @Autowired
     @Transient
     private val commandGateway: CommandGateway? = null
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    fun handle(orderCreatedEvent: OrderCreatedEvent){
+
+    }
 }
